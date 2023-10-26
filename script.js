da715af628c0ceb1e7501409fc81bdbb738ecbfc
Meryanti <148862431+Meryanti@users.noreply.github.com>
2023-10-26 22:41:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() { header.classList.toggle("sticky", window.scrollY > 120); });
 
-let menu = document.quaerySelector('#menu-icon');
+let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
